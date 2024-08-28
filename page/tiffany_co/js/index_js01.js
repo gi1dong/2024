3cbdeb20e3240eb411e65d5bda2ad5dff4504269
kimbomi <qhalqhal1231@naver.com>
2024-08-28 23:03:17 +0900

    Simplify : 	TIFFANY&CO 페이지 수정

--- page/tiffany_co/js/index_js01.js
+++ page/tiffany_co/js/index_js01.js
@@ -1,71 +1,65 @@
-$(function () {
-  AOS.init();
-  $(".header>.hamburger").click(function () {
-    $("#left_menu").stop().animate({ left: "0" }, 600);
-
-    $(this).addclass("on");
-    $(".hd_icon").addClass("on");
-    $(".header>.logo").addClass("bg_f");
-  });
-  $(".header>hamburger").click(function () {
-    if ($(this).hasclass("on")) {
+AOS.init();
+(function () {
+  $(".hamburger").click(function () {
+    if ($(this).hasClass("on")) {
       $(this).removeClass("on");
+      $(".dimmed_bg").addClass("on");
       $("#left_menu").stop().animate({ left: "-100%" }, 600);
-      $(".hd_icon").removeClass("on");
-      $(".header>.logo").removeClass("bg_f");
     } else {
-      $(this).addClass("on");
+      $(this).removeClass("on");
+      $(".dimmed_bg").removeClass("on");
       $("#left_menu").stop().animate({ left: "0" }, 600);
-      $(".hd_icon").addClass("on");
-      $(".header>.logo").addClass("bg_f");
     }
   });
   $(".close_btn").click(function () {
     $("#left_menu").stop().animate({ left: "-100%" }, 600);
   });
+
   var controller = new ScrollMagic.Controller({
     globalSceneOptions: {
-      triggerHook: "onLeave",
-      duration: "200%",
-    },
+			triggerHook: 'onLeave',
+			duration: '100%',
+		}
   });
+
   /*움직이는 txt*/
-  var txt_ani01 = TweenMax.to(".ani_wrap01 .ani_txt01", 0.7, { x: "-10%" });
-  var txt_ani02 = TweenMax.to(".ani_wrap01 .ani_txt02", 0.7, { x: "40%" });
-  var scene01 = new ScrollMagic.Scene({
+  let txt_ani01 = TweenMax.to(".ani_wrap01 .ani_txt01", 0.5, { x: "-10%" });
+  let txt_ani02 = TweenMax.to(".ani_wrap01 .ani_txt02", 0.5, { x: "40%" });
+  let scene01 = new ScrollMagic.Scene({
+    triggerHook: "onLeave",
     triggerElement: ".ani_wrap01",
-    duration: "400%",
-    offset: -500,
+    duration: "100%",
   })
-    .setTween(txt_ani01)
-    .addTo(controller);
-  var scene02 = new ScrollMagic.Scene({
+  .setTween(txt_ani01)
+  .addTo(controller)
+
+  let scene02 = new ScrollMagic.Scene({
+    triggerHook: "onLeave",
     triggerElement: ".ani_wrap01",
-    duration: "400%",
-    offset: -500,
+    duration: "100%",
   })
-    .setTween(txt_ani02)
-    .addTo(controller);
+  .setTween(txt_ani02)
+  .addTo(controller)
+
   /*tiffany note*/
-  var scene03 = new ScrollMagic.Scene({
+  let scene03 = new ScrollMagic.Scene({
     triggerElement: "#container3",
-    duration: "200%",
-    offset: 170,
+    
   })
-    .setPin("#container3", { pushFollowers: false })
-    .addTo(controller);
+  .setPin("#container3", { pushFollowers: false })
+  .addTo(controller)
+	// .addIndicators();
 
   /*bg change*/
-  var tween1 = TweenMax.to("main", 0.08, {
+  let tween1 = TweenMax.to("main", 0.08, {
     backgroundColor: "#b3f2f1",
   });
-  var scene04 = new ScrollMagic.Scene({
+
+  let scene04 = new ScrollMagic.Scene({
     triggerElement: ".ani_wrap01",
-    offset: 200,
     duration: "200%",
-  }).addIndicators({
-    name: "1"
   })
-    .setTween(tween1)
-    .addTo(controller);
-});
+  .setTween(tween1)
+  .addTo(controller)
+}());
+
